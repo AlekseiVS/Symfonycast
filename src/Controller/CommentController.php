bb496ff4758bc5e35527dd93686744fb43029443
a.sokoliuk <a.sokoliuk@devcaz.com>
2021-03-26 17:25:08 +0200

    Symfony cast start

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,13 +11,14 @@ class CommentController extends AbstractController
     /**
      * @Route ("comments/{id}/vote/{direction<up|down>}", methods="POST")
      */
-    public function commentVote($id, $direction)
+    public function commentVote($id, $direction, LoggerInterface $logger)
     {
         if ($direction == 'up') {
             $currentCoutn = rand(7,100);
-
+            $logger->info('Up');
         } else {
             $currentCoutn = rand(0,5);
+            $logger->info('Down');
         }
 
         return $this->json(['votes' => $currentCoutn]);
